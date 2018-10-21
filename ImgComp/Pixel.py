8e0069c82ee2b2bcb9da052d730d57707ea25e2d
Aakash Hemadri <aakashhemadri123@gmail.com>
2018-10-21 18:38:35 +0530

    Image Compression implementation

---
+++ ImgComp/Pixel.py
@@ -0,0 +1,9 @@
+class Pixel(object):
+    def __init__(self, color = [0, 0, 0],
+            topLeft = Point(0, 0),
+            bottomRight = Point(0, 0)):
+        self.R = color[0]
+        self.G = color[1]
+        self.B = color[2]
+        self.topLeft = topLeft
+        self.bottomRight = bottomRight
